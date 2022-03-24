ea98af87800933cd56ad5e7f8ed98cfa8db36972
Felice <felicepng2010@gmail.com>
2022-03-24 17:22:20 +0800

    Edit UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,67 +10,80 @@ const axiosInstance = axios.create({
 });
 
 const Home: NextPage = () => {
-  const [name, setName] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [dateOfBirth, setDateOfBirth] = useState<string>('');
   const [zxcvbnResult, setZxcvbnResult] = useState<IZXCVBNResult | null>(null);
 
-  const handleSubmit = async () => {
-    const response = await axiosInstance.post('/password', { password });
-    console.log(response);
-  };
-
   useEffect(() => {
-    const result = zxcvbn(password, [name]);
+    const result = zxcvbn(password, [firstName, lastName, dateOfBirth]);
     setZxcvbnResult(result);
     console.log(result);
-  }, [name, password]);
+  }, [firstName, lastName, dateOfBirth, password]);
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen p-3 font-poppins-regular">
-      <div className="flex flex-col space-y-1 items-center">
-        <input
-          className="p-3 border-2 rounded-lg"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          className="p-3 border-2 rounded-lg"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div
-          onClick={handleSubmit}
-          className="bg-green-100 w-1/2 text-center cursor-pointer rounded-md p-1 border-2"
-        >
-          Submit
-        </div>
-        <div className="flex flex-col w-full">
-          <span className="font-poppins-bold text-center">
-            Basic Password Security
-          </span>
-          <div className="flex w-full space-x-3">
-            {zxcvbnResult &&
-              zxcvbnResult.sequence.map((sequence, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="grid grid-cols-2 border text-sm gap-x-2"
-                  >
-                    <span className="font-poppins-medium">Pattern</span>
-                    <span>{sequence.pattern}</span>
-                    {sequence.l33t && (
-                      <Fragment>
-                        <span className="font-poppins-medium">
-                          L33t characters
-                        </span>
-                        <span>{sequence.sub_display as string}</span>
-                      </Fragment>
-                    )}
-                  </div>
-                );
-              })}
+    <div className="bg-primary text-white w-screen h-screen font-poppins-regular">
+      <img src="/design.png" className="top-0 right-0 absolute w-2/3 opacity-60" />
+      <div className="flex flex-col py-16 px-24 w-full h-full absolute z-10">
+        <div className="text-4xl font-poppins-semibold mt-1">Password-Strength Meter</div>
+        <div className="text-xl text-secondary mt-1">CS440 G2T8</div>
+
+        <div className="mt-16 xl:w-3/5 flex flex-col space-y-6 items-center">
+          <div className="flex justify-between w-full gap-x-6">
+            <input
+              className="w-full focus:outline-none placeholder-gray-500 bg-primary p-3 border-2 border-secondary rounded-lg"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              className="w-full focus:outline-none placeholder-gray-500 bg-primary p-3 border-2 border-secondary rounded-lg"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <input
+            className="w-full focus:outline-none placeholder-gray-500 bg-primary p-3 border-2 border-secondary rounded-lg"
+            placeholder="Date of Birth"
+            value={dateOfBirth}
+            onChange={(e) => setDateOfBirth(e.target.value)}
+          />
+          <input
+            className="w-full focus:outline-none placeholder-gray-500 bg-primary p-3 border-2 border-secondary rounded-lg"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <hr className="opacity-40 w-full" />
+          <div className="flex flex-col w-full">
+            <span className="font-poppins-bold text-center">
+              Basic Password Security
+            </span>
+            <div className="flex w-full space-x-3">
+              {zxcvbnResult &&
+                zxcvbnResult.sequence.map((sequence, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="grid grid-cols-2 border text-sm gap-x-2"
+                    >
+                      <span className="font-poppins-medium">Pattern</span>
+                      <span>{sequence.pattern}</span>
+                      {sequence.l33t && (
+                        <Fragment>
+                          <span className="font-poppins-medium">
+                            L33t characters
+                          </span>
+                          <span>{sequence.sub_display as string}</span>
+                        </Fragment>
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
       </div>
